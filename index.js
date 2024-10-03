22475e10e6d750b7aef6360c206616e6c05023dc
grace <gracemwangi84@gmail.com>
2024-10-03 19:53:49 +0300

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName = 'bob'; // Declared globally
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase(); // Modify global variable to uppercase
+}
+
+function setBestCustomer() {
+  window.bestCustomer = 'not bob'; // Set a new global variable bestCustomer
+}
+
+function overwriteBestCustomer(newCustomer) {
+  window.bestCustomer = newCustomer; // Overwrite the bestCustomer global variable
+}
+
+const leastFavoriteCustomer = 'somebody';
+ 
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'anybody else'; // Trying to reassign a constant will throw an error
+}
